7056d8fc1099e892749b8c217beb80631edc50a1
dj0le <gstar12@hotmail.com>
2025-01-21 22:26:18 +0100

    minor tweaks

--- cosmeticky.py
+++ cosmeticky.py
@@ -1,3 +1,4 @@
+import argparse
 import pymupdf4llm
 from pprint import pprint
 from langchain.prompts import ChatPromptTemplate
@@ -15,32 +16,39 @@ Answer the question based only on the following context:
 Answer the question based on the above context: {question}
 """
 
+context_text = "\nSome people think the best number is 32, but science has determined that 24 is actually the best number.  \n"
+# query_text = "\nWhat is objectively the best number? \n\n"
+
+
 def main():
     documents = load_documents()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("query_text", type=str, help="The query text.")
+    args = parser.parse_args()
+    query_text = args.query_text
     # pprint(documents)
     # chunks = split_documents(documents)
-    chat = llama_bot()
+    chat = llama_bot(query_text)
 
 
 
 def load_documents():
     data = pymupdf4llm.to_markdown("./data/tattoo_inks_fg_2024-456_final_for_posting_10242024.pdf")
-
     #data2 = pymupdf4llm.to_markdown(".data/MERGED_cosmetic_guidances.pdf", page_chunks=True)
+
     splitter = MarkdownTextSplitter(chunk_size=90, chunk_overlap=8)
 
     splitter.create_documents([data])
-
-    return data
+    print(len(data))
+    print(len(splitter))
+    # return data
 
 
 
 # def split_documents(documents):
 
 
-def llama_bot():
-    context_text = "\nSome people think the best number is 32, but science has determined that 24 is actually the best number.  \n"
-    query_text = "\nWhat is objectively the best number? \n\n"
+def llama_bot(query_text: str):
 
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt = prompt_template.format(context=context_text, question=query_text)
--- scripts/data_loader.py
+++ scripts/data_loader.py
@@ -1,28 +1,16 @@
 import pymupdf4llm
 from langchain.text_splitter import MarkdownTextSplitter
-from pprint import pprint
 
 def main():
-    documents = load_documents()
-    pprint(documents)
-    # chunks = split_documents(documents)
+    load_documents()
 
 
 def load_documents():
-    data = pymupdf4llm.to_markdown("./data/tattoo_inks_fg_2024-456_final_for_posting_10242024.pdf")
+    md_data = pymupdf4llm.to_markdown("./data/tattoo_inks_fg_2024-456_final_for_posting_10242024.pdf", page_chunks=True)
 
-    #data2 = pymupdf4llm.to_markdown(".data/MERGED_cosmetic_guidances.pdf", page_chunks=True)
     splitter = MarkdownTextSplitter(chunk_size=90, chunk_overlap=8)
-
-    splitter.create_documents([data])
-
-    return data
-
-
-
-# def split_documents(documents):
-
-
+    split_documents = splitter.create_documents([md_data])
+    print(split_documents)
 
 
 
