5f2fc21eec067210662597160fb4d0449526f59f
dj0le <gstar12@hotmail.com>
2025-02-15 16:28:35 +0100

    updated styling on toast component

--- frontend/src/lib/components/toast.svelte
+++ frontend/src/lib/components/toast.svelte
@@ -15,9 +15,12 @@
 	let showToast = $state(false);
 	let timeoutId: number | undefined;
 
+	let iconPath = $derived(`./${type}.svg`);
+
 	$effect(() => {
 		if (message) {
 			showToast = true;
+
 			timeoutId = setTimeout(() => {
 				showToast = false;
 			}, duration);
@@ -32,31 +35,58 @@
 
 {#if showToast}
 	<div class="toast {type}" transition:fade={{ duration: 300 }}>
-		{message}
+		<div class="toast-icon {type}">
+			<img src={iconPath} alt={type} />
+		</div>
+		<div class="toast-message">{message}</div>
 	</div>
 {/if}
 
 <style>
 	.toast {
 		position: fixed;
-		top: 40px;
-		right: 40px;
-		background-color: var(--surface-2);
-		color: var(--text-2);
-		padding: 12px 24px;
+		top: 32px;
+		right: 32px;
+		display: grid;
+		grid-template-columns: auto 1fr;
+		align-items: center;
 		border-radius: 6px;
+		overflow: hidden;
+		margin-bottom: 10px;
 	}
-
+	.toast-message {
+		padding: 10px 20px;
+		background: var(--surface-2);
+		word-break: break-word;
+	}
+	.toast-icon {
+		padding: 10px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
 	.toast.success {
-		background-color: var(--success);
+		border: 2px solid var(--success);
 	}
 	.toast.error {
-		background-color: var(--error);
+		border: 2px solid var(--error);
 	}
 	.toast.info {
-		background-color: var(--info);
+		border: 2px solid var(--info);
 	}
 	.toast.warning {
+		border: 2px solid var(--warning);
+	}
+	.toast-icon.warning {
 		background-color: var(--warning);
+	}
+	.toast-icon.error {
+		background-color: var(--error);
+	}
+	.toast-icon.success {
+		background-color: var(--success);
+	}
+	.toast-icon.info {
+		background-color: var(--info);
 	}
 </style>
