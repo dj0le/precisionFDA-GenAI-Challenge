6c78379ff273df820405f40c3b0c105f53edf29e
dj0le <gstar12@hotmail.com>
2025-02-17 15:23:25 +0100

    style welcome text

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -70,46 +70,72 @@
 
 <div class="chat-container">
 	{#if !hasAskedQuestion}
-		<div class="placeholder-message">
-			<h3 class="title">Hello, I'm Feddi</h3>
-			<p>
-				I'm your guide to FDA related questions. If this is your first time using the app, here's a
-				few tips:
-			</p>
-			<p>
-				On the top right, select the chat model you w to use. I'm a fan ofllama 3.2, but any chat
-				model will work, and you can change it anytime.
-			</p>
-			<p>
-				There are 3 sections to this app, this one is for chatting. If you would like to add
-				documents to my database, choose the documents section. You can also see all the documents
-				currentlyin the database, or delete any that are no longer needed.
-			</p>
-			<p>
-				The info section will give more details about the project and participants. You can also
-				verify that the app is up and running (look for th healthy green heart). And, you can see a
-				list of all the local models on your system that the app can see to ensure the ones you want
-				are available.
-			</p>
-			<p>
-				When ready, simply come back to this chat page, and at the bottom input whatever questions
-				you have
-			</p>
-			<p>Example questions:</p>
-			<ul>
-				<li>
-					How does the FDA define "insanitary conditions" in the preparation, packing, and holding
-					of tattoo inks?
-				</li>
-				<li>What is the numeric code for eye liner?</li>
-				<li>What are common mistakes with non compliance when starting a cosmetic business??</li>
-			</ul>
-			<p>
-				I will always attempt to use the local documents to answer your questions, to ensure as much
-				accuracy as possible
-			</p>
-			<p>I look forward to interacting with you!</p>
-			<h3 class="title">-Feddi</h3>
+		<div class="placeholder">
+			<div>
+				<h2 class="title">Hello, I'm Feddi</h2>
+
+				<p class="gap">
+					I'm your guide to FDA related questions. If this is your first time using the app, here's
+					a few tips:
+				</p>
+				<div class="model-info">
+					<p class="sub-title gap">1. Select a model</p>
+					<p>
+						On the top right, select the chat model you want to use. I'm a fan of Llama 3.2, but any
+						chat model will work. Change it anytime.
+					</p>
+					<p class="sub-title gap">2. Upload your documents</p>
+					<p>
+						If you haven't already, you'll need to add your pdf documents to the database in the
+						document section.
+					</p>
+					<p class="sub-title gap">3. Ask your questions</p>
+					<p>
+						Once you've selected a model, you can start asking questions. I'll do my best to provide
+						you with accurate and helpful information.
+					</p>
+				</div>
+				<div>
+					<p class="sub-title gap">Examples</p>
+					<ul>
+						<li>
+							How does the FDA define "insanitary conditions" in the preparation, packing, and
+							holding of tattoo inks?
+						</li>
+						<li>What is the numeric code for eye liner?</li>
+						<li>
+							What are common mistakes with non compliance when starting a cosmetic business??
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="column-right">
+				<h3 class="title">The app has 3 sections:</h3>
+				<div class="chat-info">
+					<p class="sub-title gap">1. Chat</p>
+					<p>
+						This the main page and where you can ask me your questions. Use the input at the bottom.
+					</p>
+				</div>
+				<div class="document-info">
+					<p class="sub-title gap">2. Documents</p>
+					<p>
+						This is where you can add or delete documents. All current documents will be displayed
+						in the list. Use the search feature to find specific documents. You can also sort by
+						name or upload date.
+					</p>
+				</div>
+				<div class="info-info">
+					<p class="sub-title gap">3. Info</p>
+					<p>
+						Details the project and participants. You can also verify that the app is up and running
+						(look for th healthy green heart). And, you can see a list of all the local models on
+						your system.
+					</p>
+				</div>
+				<p class="gap">I look forward to interacting with you!</p>
+				<h3 class="title gap sig">-Feddi</h3>
+			</div>
 		</div>
 	{:else if isLoading}
 		<div>thinking...</div>
@@ -166,6 +192,22 @@
 </div>
 
 <style>
+	.gap {
+		margin-top: 16px;
+	}
+	.sig {
+		text-align: right;
+		padding-right: 32px;
+	}
+	.sub-title {
+		font-size: var(--font-size-sub-title);
+	}
+	.placeholder {
+		padding: 16px;
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		gap: 32px;
+	}
 	.chat-container {
 		border: 1px solid var(--border-1);
 		background-color: var(--surface-2);
